3997a27103385099503320a1c3fcfea0b0026a5e
Hkunnath <honeyk.mec@gmail.com>
2024-01-22 12:06:23 +0100

    VNGRS1-28 - Unit test for Authentication

--- src/main/java/com/example/ecommerceshop/user/model/User.java
+++ src/main/java/com/example/ecommerceshop/user/model/User.java
@@ -4,8 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
-
 
 
 @Entity
