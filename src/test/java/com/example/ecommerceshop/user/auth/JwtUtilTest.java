383aa3ea80ec7d3c14501f4fa33142eefd34b409
Hkunnath <honeyk.mec@gmail.com>
2024-01-23 10:48:44 +0100

    VNGRS1-updated unit test of jwtUtil

--- src/test/java/com/example/ecommerceshop/user/auth/JwtUtilTest.java
+++ src/test/java/com/example/ecommerceshop/user/auth/JwtUtilTest.java
@@ -2,8 +2,10 @@ package com.example.ecommerceshop.user.auth;
 
 import com.example.ecommerceshop.user.model.User;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -26,6 +28,12 @@ class JwtUtilTest {
     private JwtUtil jwtUtil;
     private SecretKey secretKey;
 
+    @Mock
+    private HttpServletRequest mockRequest;
+
+    @Mock
+    private Claims mockClaims;
+
     @BeforeEach
     void setUp(){
         secretKey = Keys.hmacShaKeyFor("MY=SUPER=SECRET=CODE123=333====2323=123==452345=6756=76=".getBytes());
@@ -39,7 +47,7 @@ class JwtUtilTest {
 
         Claims claims = Jwts.claims().subject(mockUser.getUsername()).build();
         Date tokenCreateTime = new Date();
-        long accessTokenValidity = 60 ;
+        long accessTokenValidity = 60 * 7 ;
 
         Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(accessTokenValidity));
 
@@ -52,5 +60,38 @@ class JwtUtilTest {
                 .compact();
 
         assertEquals(token,returnString);
+    }
+
+    @Test
+    void resolveToken(){
+        final String TOKEN_HEADER = "Authorization";
+        final String TOKEN_PREFIX = "Bearer ";
+        final String validToken = "token";
+        when(mockRequest.getHeader(TOKEN_HEADER)).thenReturn(TOKEN_PREFIX + validToken);
+        assertEquals(validToken,jwtUtil.resolveToken(mockRequest));
+    }
+
+    @Test
+    void validClaims(){
+        Date futureExpiration = new Date(System.currentTimeMillis() + 60);
+        when(mockClaims.getExpiration()).thenReturn(futureExpiration);
+        assertTrue(jwtUtil.validateClaims(mockClaims));
+    }
+
+    @Test
+    void nullClaimsThrowsException(){
+       Exception exception = assertThrows(IllegalArgumentException.class,()->jwtUtil.validateClaims(null));
+        String expectedMessage = "Invalid claims or expiration";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void nullExpirationThrowsException(){
+        when(mockClaims.getExpiration()).thenReturn(null);
+        Exception exception = assertThrows(IllegalArgumentException.class,()->jwtUtil.validateClaims(null));
+        String expectedMessage = "Invalid claims or expiration";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
     }
 }
