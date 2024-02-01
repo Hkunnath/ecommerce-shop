8e08704056f9ac6f788a71bcf91e4b7d5f999b7b
Hkunnath <honeyk.mec@gmail.com>
2024-02-01 12:55:49 +0100

    updated orderservice

--- src/main/java/com/example/ecommerceshop/order/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerceshop/order/repository/OrderRepository.java
@@ -4,5 +4,8 @@ import com.example.ecommerceshop.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.relational.core.sql.In;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Optional<Order> findByUserId(Integer id);
 }
--- src/main/java/com/example/ecommerceshop/order/service/OrderService.java
+++ src/main/java/com/example/ecommerceshop/order/service/OrderService.java
@@ -1,10 +1,20 @@
 package com.example.ecommerceshop.order.service;
 
+import com.example.ecommerceshop.cart.model.Cart;
+import com.example.ecommerceshop.cart.repository.CartRepository;
+import com.example.ecommerceshop.order.dto.OrderDto;
+import com.example.ecommerceshop.order.model.Order;
+import com.example.ecommerceshop.order.model.OrderStatus;
 import com.example.ecommerceshop.order.repository.OrderRepository;
 import com.example.ecommerceshop.user.auth.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +26,9 @@ public class OrderService {
         final String token = jwtUtil.getToken(bearerToken);
         final Integer userId = jwtUtil.getUserIdFromToken(token);
         log.info("User id" + userId);
-    }
+        Optional<Order> order = orderRepository.findByUserId(userId);
+        if(order.isEmpty()){
+            log.info("no orders yet");
+            }
+        }
 }
